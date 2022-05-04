9f08877374bad4b722b139350f9eb3fbed6d96ab
Matilde Costa <matildecosta@MacBook-Pro-de-Matilde.local>
2022-05-04 10:02:16 +0100

    working on 'my work' page

--- public/scripts/buttons.js
+++ public/scripts/buttons.js
@@ -1,9 +1,12 @@
 const playButton = document.getElementsByClassName("play-button")[0];
 const pauseButton = document.getElementsByClassName("pause-button")[0];
+const stopButton = document.getElementsByClassName("stop-button")[0];
+
 const audio = new Audio();
 
 playButton.addEventListener("click", () => {
   console.log("hello");
   playButton.style.display = "none";
   pauseButton.style.display = "flex";
+  stopButton.style.display = "flex";
 });
